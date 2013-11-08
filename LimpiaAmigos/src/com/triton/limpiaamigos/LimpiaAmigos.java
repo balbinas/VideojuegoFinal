c7a9147a79aac5e60a46ef46031873e6de4d8fa5
Jose Luis Loya <jl.loya@gmail.com>
2013-11-07 20:00:58 -0600

    Importe / Arregle y Factorize todo el codigo ejemplo de Mejorado

---
+++ LimpiaAmigos/src/com/triton/limpiaamigos/LimpiaAmigos.java
@@ -0,0 +1,18 @@
+package com.triton.limpiaamigos;
+
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import com.triton.tilegame.*;
+
+public class LimpiaAmigos extends GameManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new GameManager().run();
+    }
+    
+    
+}
